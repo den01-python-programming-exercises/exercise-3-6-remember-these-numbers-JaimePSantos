13d85024310799ea7b22488ef3736a94cd2cc0b1
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 20:32:22 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    intList = []
+    while(True):
+      number = int(input())
+      if(number==-1):
+        break
+      intList.append(number)
+    for i in range(len(intList)):
+      print(intList[i])
 
 if __name__ == '__main__':
     main()
